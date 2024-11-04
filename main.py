a87b95f582f1893b1f797a4ead896293bc8c8205
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2024-11-04 12:52:57 -0600

    feat: manipulate Task class through argparse commands

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 import argparse
-
-
-class Task:
-
-    index: int = 0
-
-    def __init__(
-        self,
-        title: str,
-        completed = True
-    ) -> None:
-        self.title: str = title
-        self.index = Task.index
-        Task.index += 1
-
+from task import Task
 
 parser = argparse.ArgumentParser(
-    prog="cli-todo", description="A simple Todo list in the CLI"
+    prog="Todo", description="A simple Todo list in the CLI"
 )
 
 command_subparser = parser.add_subparsers(title="Commands", dest="command", description="Available Commands", required=True, metavar="")
 
 create_parser = command_subparser.add_parser("create", help="Create a new Task")
-create_parser.add_argument("name", type=str, help="Name of the task")
+create_parser.add_argument("title", type=str, help="Title of the task")
 create_parser.add_argument("-d", "--description", type=str, help="Description of the task")
 
 delete_parser = command_subparser.add_parser("delete", help="Delete a task")
@@ -33,4 +19,32 @@ complete_parser.add_argument("id", type=int, help="Id of the Task to mark as com
 
 list_parser = command_subparser.add_parser("list", help="List all current Tasks")
 
-parser.parse_args()
+args = parser.parse_args()
+
+command: str = args.command
+
+Task.create("Task1", "A small description")
+Task.create("Task2", None)
+
+match command:
+    case "create":
+        task = Task.create(args.title, args.description)
+        print("New Task Created\n", task)
+
+    case "delete":
+        task: Task | bool = Task.delete(args.id)
+
+        if task:
+            print(f"Task {args.id} Deleted Succesfully\n\n", task)
+        else:
+            parser.exit(message=f"Error: Task with id: \"{args.id}\" does not exist", status=2)
+
+    case "complete":
+        task: Task | bool = Task.complete(args.id)
+        if task:
+            print(f"Task {args.id} marked as compelted\n\n",task)
+        else:
+            parser.exit(message=f"Task with id: \"{args.id}\" does not exist", status=2)
+
+    case "list":
+        Task.list()
