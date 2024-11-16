2a0071f2f5eab399e4e5f1242ddf8ff6a1135cff
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2024-11-16 00:15:01 -0600

    feat: add python-dotenv dependency

--- src/logger.py
+++ src/logger.py
@@ -2,23 +2,22 @@ import logging
 from rich.logging import RichHandler
 
 LOGER_NAME = "todolist"
+CONSOLE_LOG_LEVEL = "CRITICAL"
+FILE_LOG_LEVEL = "DEBUG"
 
-def setup_logger(debug: bool = False) -> None:
-    LOG_FILE = f"{LOGER_NAME}.log"
-
+def setup_logger(console_level: str = CONSOLE_LOG_LEVEL, file_level: str = FILE_LOG_LEVEL) -> None:
     logger = logging.getLogger(LOGER_NAME)
     logger.setLevel(logging.DEBUG)
 
     # File handler
-    file_handler = logging.FileHandler(f"../{LOG_FILE}")
-    file_handler.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(f"../{f"{LOGER_NAME}.log"}")
+    file_handler.setLevel(file_level)
     file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     file_handler.setFormatter(file_formatter)
 
     # Console handler
-    debug_level = logging.CRITICAL if not debug else logging.DEBUG
     console_handler = RichHandler(markup=True, rich_tracebacks=True, tracebacks_show_locals=True)
-    console_handler.setLevel(debug_level)
+    console_handler.setLevel(console_level)
     console_formatter = logging.Formatter("%(levelname)s: %(message)s")
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
